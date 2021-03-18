eb97c92266e320abab4e4056f3ecb99e69fe0b0e
camryn williams <williams.camryn.e@gmail.com>
2021-03-18 11:13:42 -0400

    commit tree 843823d5ea1c3f3d2bd4285366521580d7e9b967

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(beep = "*") {
+    
+    return function(boop = "special"){
+        return `You are ${beep}${boop}${beep}!`;
+    }
+}
